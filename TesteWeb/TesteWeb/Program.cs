37116d574f4c495ff54b99e5495a46a6585b25aa
Beatriz Pires <anabeatrizsousaps@gmail.com>
2025-09-23 14:12:53 -0300

    MYSQL Adaptation and first migration

--- TesteWeb/TesteWeb/Program.cs
+++ TesteWeb/TesteWeb/Program.cs
@@ -1,26 +1,20 @@
-using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Data;
-namespace SalesWebMvc
+namespace TesteWeb
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-                options.UseMySQL(builder.Configuration.GetConnectionString("SalesWebMvcContext"), builder => builder.MigrationsAssembly("SalesWebMvc")));
-            //Injeção de dependência
-
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -32,9 +26,7 @@ namespace SalesWebMvc
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
 
             app.Run();
         }
