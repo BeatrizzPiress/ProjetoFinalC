e47fb57eff3e4954257bb340a0b3f731f3c65872
Beatriz Pires <anabeatrizsousaps@gmail.com>
2025-09-25 13:45:45 -0300

    Simple Create Form

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMvc.Models.Enums;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace SalesWebMvc.Models
@@ -12,6 +13,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
